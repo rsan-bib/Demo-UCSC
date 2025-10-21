9a7fbab06165625e57264e11c4445ad61fd5deb0
Rsanchezm <capitalcatto@gmail.com>
2025-10-20 23:49:59 -0300

    parte 9

--- Library/PackageCache/com.unity.render-pipelines.core@05f02f04fe74/Runtime/RenderPipeline/ICloudBackground.cs
+++ Library/PackageCache/com.unity.render-pipelines.core@05f02f04fe74/Runtime/RenderPipeline/ICloudBackground.cs
@@ -1,15 +1,3 @@
-
-namespace UnityEngine.Rendering
-{
-    /// <summary>
-    /// Interface defining if an SRP supports environment effects for lens flare occlusion
-    /// </summary>
-    public interface ICloudBackground
-    {
-        /// <summary>
-        /// Check is the current Render Pipeline supports environement effects for lens flare occlusion.
-        /// </summary>
-        /// <returns>true if environement effects may occlude lens flares.</returns>
-        public bool IsCloudBackgroundUsable();
-    }
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:c3b73acec5327a92f7f2c26343132405fe30bbb1818ab574817f2c0e4beac62f
+size 497
--- Library/PackageCache/com.unity.shadergraph@7ccd16edcc88/Editor/Data/Attributes/NeverAllowedByTargetAttribute.cs
+++ Library/PackageCache/com.unity.shadergraph@7ccd16edcc88/Editor/Data/Attributes/NeverAllowedByTargetAttribute.cs
@@ -1,9 +1,3 @@
-using System;
-
-namespace UnityEditor.ShaderGraph
-{
-    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
-    internal class NeverAllowedByTargetAttribute : ContextFilterableAttribute
-    {
-    }
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:495832487017d7e5420e07b433d1e30e99d1ef5996570fd4bdeeff6d27956c25
+size 230
